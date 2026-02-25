0a116af355f9babac5adba3f238c8e52bb60d568
Mateus Soares <mateus.silvainfo@gmail.com>
2026-02-25 19:17:10 -0300

    feat: add nest microservices lib and instantiate it

--- apps/logging-service/src/main.ts
+++ apps/logging-service/src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { LoggingServiceModule } from './logging-service.module';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
-  const app = await NestFactory.create(LoggingServiceModule);
-  await app.listen(process.env.port ?? 3002);
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    LoggingServiceModule,
+    {
+      transport: Transport.RMQ,
+      options: {
+        urls: [process.env.RABBITMQ_URL ?? 'amqp://localhost:5672'],
+        queue: 'logging_queue',
+        queueOptions: {
+          durable: false,
+        },
+      },
+    },
+  );
+  await app.listen();
+  console.log('Logging service is listening...');
 }
 bootstrap();
--- apps/user-service/src/main.ts
+++ apps/user-service/src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { UserServiceModule } from './user-service.module';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
-  const app = await NestFactory.create(UserServiceModule);
-  await app.listen(process.env.port ?? 3001);
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    UserServiceModule,
+    {
+      transport: Transport.RMQ,
+      options: {
+        urls: [process.env.RABBITMQ_URL ?? 'amqp://localhost:5672'],
+        queue: 'user_queue',
+        queueOptions: {
+          durable: false,
+        },
+      },
+    },
+  );
+  await app.listen();
+  console.log('User service is listening...');
 }
 bootstrap();
