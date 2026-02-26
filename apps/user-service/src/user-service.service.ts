16c1e8f79bd2e82cdf58bca42995549ef03aadbb
Mateus Soares <mateus.silvainfo@gmail.com>
2026-02-26 00:47:30 -0300

    feat: create user service module, controller and service

--- apps/user-service/src/user-service.service.ts
+++ apps/user-service/src/user-service.service.ts
@@ -1,8 +1,29 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { ClientProxy } from '@nestjs/microservices';
 
 @Injectable()
 export class UserServiceService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(
+    @Inject('LOGGING_SERVICE') private readonly loggingClient: ClientProxy,
+  ) {}
+
+  async create(dto: any) {
+    const user = { id: 1, ...dto };
+
+    // this.loggingClient.emit(MESSAGES.LOG_EVENT, {
+    //   action: 'user.created',
+    //   payload: { email: dto.email },
+    //   timestamp: new Date(),
+    // });
+
+    return user;
+  }
+
+  findAll() {
+    return [];
+  }
+
+  findOne(id: number) {
+    return { id };
   }
 }
